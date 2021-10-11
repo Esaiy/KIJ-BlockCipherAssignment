9fa358466b9ad8a7233eac9fd819744b62033620
Alberto0150 <albertosanjaya2@gmail.com>
2021-10-11 22:33:56 +0700

    Benarin mixcolumn

--- decrypt/aes_decrypt.go
+++ decrypt/aes_decrypt.go
@@ -122,6 +122,24 @@ func Aes_decrypt_scratch_shiftrows(ready_to_change_2_Dim []byte) [][]byte {
 	return new_array
 }
 
+func multiply_by_2(the_value byte) byte {
+	var new_value byte
+	new_value = the_value << 1
+	new_value = new_value & 0xff
+
+	if (new_value & 128) != 0 {
+		new_value = new_value ^ 0x1b
+	}
+
+	return new_value
+}
+
+func multiply_by_3(the_value byte) byte {
+	var the_result byte
+	the_result = multiply_by_2(the_value) ^ the_value
+	return the_result
+}
+
 func Aes_decrypt_scratch_mixcolumn(thirdstep_array [][]byte) [][]byte {
 	// reference: https://gist.github.com/vwxyzjn/bcac5f97b5abb7708773a28b82a809b4
 	// reference: https://blog.tclaverie.eu/posts/understanding-golangs-aes-implementation-t-tables/
@@ -132,11 +150,18 @@ func Aes_decrypt_scratch_mixcolumn(thirdstep_array [][]byte) [][]byte {
 		{01, 01, 02, 03},
 		{03, 01, 01, 02},
 	}
+
 	var new_array [][]byte
 	for i := 0; i < 4; i++ {
 		for j := 0; j < 4; j++ {
 			for k := 0; k < 4; k++ {
-				new_array[i][j] = static_matrix[i][k] * thirdstep_array[k][j]
+				if static_matrix[i][k] == 2 {
+					new_array[i][j] = multiply_by_2(thirdstep_array[k][j])
+				} else if static_matrix[i][k] == 3 {
+					new_array[i][j] = multiply_by_3(thirdstep_array[k][j])
+				} else {
+					new_array[i][j] = thirdstep_array[k][j]
+				}
 			}
 		}
 	}
